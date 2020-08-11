8653c9a7f53fd2de57ddac4afaa8aa1bd528c88c
boyercb <christopherbboyer@gmail.com>
2020-08-10 22:49:04 -0400

    define parametric models

---
+++ 5_parametric_models/1_fit_parametric_models.R
@@ -0,0 +1,42 @@
+
+# Fit parametric models ---------------------------------------------------
+
+Y.fit <- 
+  fit_Y_model(
+    Y.model,
+    data = analytic_long
+  )
+
+X.fit <- 
+  fit_X_models(
+    X.models,
+    data = analytic_long,
+    time = "time"
+  )
+
+D.fit <- 
+  fit_D_model(
+    D.model,
+    data = analytic_long
+  )
+
+
+# Create output tables ----------------------------------------------------
+
+sink("9_results/tables/cov_models.tex")
+texreg(
+  l = X.fit,
+  booktabs = TRUE,
+  use.packages = FALSE,
+  table = FALSE
+) %>% print()
+sink()
+
+sink("9_results/tables/out_models.tex")
+texreg(
+  l = list(Y.fit, D.fit),
+  booktabs = TRUE,
+  use.packages = FALSE,
+  table = FALSE
+) %>% print()
+sink()

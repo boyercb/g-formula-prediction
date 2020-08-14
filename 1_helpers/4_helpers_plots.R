7fbbb09b7ba5cb3c49c8e274e77fd5f9e9067b62
boyercb <christopherbboyer@gmail.com>
2020-08-14 07:57:29 -0400

    create separate base themes for latex and beamer slides

--- 1_helpers/4_helpers_plots.R
+++ 1_helpers/4_helpers_plots.R
@@ -1,4 +1,4 @@
-g_theme <-
+latex_theme <-
   function() {
     theme_bw() +
       theme(
@@ -9,5 +9,19 @@ g_theme <-
         strip.background = element_blank(),
         legend.position = "bottom",
         text = element_text(family = "Palatino")
+      )
+  }
+
+slides_theme <-
+  function() {
+    theme_bw() +
+      theme(
+        #axis.ticks = element_blank(),
+        #axis.line = element_blank(),
+        #panel.border = element_blank(),
+        panel.grid = element_blank(), #element_line(color = '#eeeeee'),
+        strip.background = element_blank(),
+        legend.position = "bottom",
+        text = element_text(family = "Helvetica")
       )
   }

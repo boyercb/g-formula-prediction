a44b18eaff6efd3ed84f32395da5b6eaad1696f5
boyercb <christopherbboyer@gmail.com>
2020-08-17 17:22:26 -0400

    update dgp based on MSM paper

--- 6_simulation/0_dgp.R
+++ 6_simulation/0_dgp.R
@@ -1,56 +1,60 @@
 # define data generation process ------------------------------------------
 
 datagen <- function(N, 
-                    a0 = -1.5,
+                    a0 = 0,
                     a1 = 1,
-                    a2 = 1, 
-                    a3 = 2,
+                    a2 = -3, 
+                    a3 = 0,
                     a4 = 0,
-                    # b0 = -3,
-                    # b1 = -1,
-                    # b2 = 0.75,
-                    # b3 = 0.2,
-                    c0 = -2,
-                    c1 = -2,
-                    c2 = 3,
-                    c3 = 0,
-                    d0 = 0,
-                    d1 = 1,
-                    d2 = 1) {
+                    b0 = log(0.25),
+                    b1 = log(2),
+                    b2 = 0,
+                    b3 = log(2),
+                    c0 = log(0.25),
+                    c1 = log(1.5),
+                    c2 = log(0.5),
+                    c3 = log(1.5),
+                    c4 = log(0.5), 
+                    sigma = 1, 
+                    type = "long") {
   
   # time point 1
-  L0 <- rbinom(N, 1, plogis(d0))
-  A0 <- rbinom(N, 1, plogis(a0 + a1 * L0))
-  #D0 <- rbinom(N, 1, plogis(b0 + b1 * A1 + b2 * L1))
-  Y0 <- rbinom(N, 1, plogis(c0 + c1 * A0 + c2 * L0)) 
-  
-  #Y0 <- ifelse(D0 == 1, NA, Y0)
+  L0 <- rnorm(N, a0, sigma)
+  A0 <- rbinom(N, 1, plogis(b0 + b1 * L0))
+  Y0 <- rbinom(N, 1, plogis(c0 + c1 * L0 + c2 * A0)) 
   
   # time point 2
-  L1 <- rbinom(N, 1, plogis(d0 + d1 * L0 + d2 * A0))
-  A1 <- rbinom(N, 1, plogis(a0 + a1 * L1 + a2 * A0 + a3 * L0 + a4))
-  #D1 <- rbinom(N, 1, plogis(b0 + b1 * A2 + b2 * L2 + b3))
-  Y1 <- rbinom(N, 1, plogis(c0 + c1 * A1 + c2 * L1 + c3))
+  L1 <- rnorm(N, a0 + a1 * L0 + a2 * A0 + a3 * L0 * A0, sigma)
+  A1 <- rbinom(N, 1, plogis(b0 + b1 * L1 + b2 * L0 + b3 * A0))
+  Y1 <- rbinom(N, 1, plogis(c0 + c1 * L1 + c2 * A1 + c3 * L0 + c4 * A0))
   
   # adjust for survival
   L1 <- ifelse(Y0 == 1 , NA, L1)
   A1 <- ifelse(Y0 == 1 , NA, A1)
-  #D1 <- ifelse(Y0 == 1 | D0 == 1, NA, D2)
   Y1 <- ifelse(Y0 == 1, 1, Y1)
-  #Y2 <- ifelse(D2 == 1, NA, Y2)
-  
-  # export simulated data
-  data.frame(
+
+  # export simulated data in either wide or long format
+  df <- switch(
+    type,
+    "long" = data.frame(
+      id = rep(1:N, 2),
+      time = rep(c(0, 1), each = N),
+      L = c(L0, L1),
+      A = c(A0, A1),
+      Y = c(Y0, Y1)
+    ),
+    "wide" = data.frame(
       id = 1:N,
-      L0 = L0, 
+      L0 = L0,
       A0 = A0,
-      #D0 = D0,
       Y0 = Y0,
       L1 = L1,
       A1 = A1,
-      #D1 = D1,
       Y1 = Y1
     )
+  )
+  
+  return(df)
 }
 
 

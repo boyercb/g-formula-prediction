7008edbef82fbd1c9bc84aea1e6036918ddc44cd
boyercb <christopherbboyer@gmail.com>
2020-09-17 23:18:28 -0400

    use time 0 as first time in sim

--- 6_simulation/3_dgp_risk_factors.R
+++ 6_simulation/3_dgp_risk_factors.R
@@ -3,30 +3,30 @@ datagen <- function(N, k, alpha, beta, gamma, sigma, output = "both") {
   W <- matrix(nrow = N, ncol = k)
   Y <- matrix(nrow = N, ncol = k)
   
-  colnames(X) <- paste0("X", 1:k)
-  colnames(W) <- paste0("W", 1:k)
-  colnames(Y) <- paste0("Y", 1:k)
+  colnames(X) <- paste0("X", 0:(k-1))
+  colnames(W) <- paste0("W", 0:(k-1))
+  colnames(Y) <- paste0("Y", 0:(k-1))
   
-  for (i in 1:k) {
-    if (i == 1) {
+  for (i in 0:(k-1)) {
+    if (i == 0) {
       lag1_X <- 0
       lag1_W <- 0
     } else {
-      lag1_X <- ifelse(is.na(X[, i - 1]), 0, X[, i - 1])
-      lag1_W <- ifelse(is.na(W[, i - 1]), 0, W[, i - 1])
+      lag1_X <- ifelse(is.na(X[, i]), 0, X[, i])
+      lag1_W <- ifelse(is.na(W[, i]), 0, W[, i])
     }
     
-    X[, i] <-
+    X[, i + 1] <-
       rnorm(N, alpha[1] + alpha[2] * lag1_X + alpha[3] * lag1_W, sigma)
-    W[, i] <-
-      rbinom(N, 1, plogis(beta[1] + beta[2] * X[, i] + beta[3] * lag1_X + beta[4] * lag1_W))
-    Y[, i] <-
-      rbinom(N, 1, plogis(gamma[1] + gamma[2] * X[, i] + gamma[3] * W[, i]))
+    W[, i + 1] <-
+      rbinom(N, 1, plogis(beta[1] + beta[2] * X[, i + 1] + beta[3] * lag1_X + beta[4] * lag1_W))
+    Y[, i + 1] <-
+      rbinom(N, 1, plogis(gamma[1] + gamma[2] * X[, i + 1] + gamma[3] * W[, i + 1]))
     
-    if (i > 1) {
-      X[, i] <- ifelse(Y[, i - 1] == 1, NA, X[, i])
-      W[, i] <- ifelse(Y[, i - 1] == 1, NA, W[, i])
-      Y[, i] <- ifelse(Y[, i - 1] == 1, 1, Y[, i])
+    if (i > 0) {
+      X[, i + 1] <- ifelse(Y[, i] == 1, NA, X[, i + 1])
+      W[, i + 1] <- ifelse(Y[, i] == 1, NA, W[, i + 1])
+      Y[, i + 1] <- ifelse(Y[, i] == 1, 1, Y[, i + 1])
     }
   }
   
@@ -35,7 +35,7 @@ datagen <- function(N, k, alpha, beta, gamma, sigma, output = "both") {
       output,
       "long" = data.frame(
         id = rep(1:N, k),
-        time = rep(1:k, each = N),
+        time = rep(0:(k-1), each = N),
         X = as.vector(X),
         W = as.vector(W),
         Y = as.vector(Y),
